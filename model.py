73d518b56027743cc7c9ccb640f72563f9fd23f8
Komputer 4 <komputer4@gmail.com>
2023-01-30 19:13:46 +0100

    used the built-in nn.linear instead of doing it by hand

--- model.py
+++ model.py
@@ -1,22 +1,15 @@
 import torch
 from torch import nn
 
-# The model has only two nodes (one in the input layer, one in the output layer)
-# because it's supposed to predict a linear formula, which can be represented
-# with this model
-
 
 class LinearRegressionModel(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.weights = nn.Parameter(torch.randn(1,
-                                                dtype=torch.float),
-                                    requires_grad=True)
-
-        self.bias = nn.Parameter(torch.randn(1,
-                                             dtype=torch.float),
-                                 requires_grad=True)
+        # The model has only two nodes (one in the input layer, one in the output layer)
+        # because it's supposed to predict a linear formula, which can be represented
+        # with this model
+        self.linear_layer = nn.Linear(in_features=1, out_features=1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.weights * x + self.bias
+        return self.linear_layer(x)
